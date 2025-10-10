a8dbc341df608837a407026a60ab8e5b569e20a3
hieuu123 <140571471+hieuu123@users.noreply.github.com>
2025-10-10 15:34:03 +0700

    Update hamster_cipher_update.py

--- hamster_cipher_update.py
+++ hamster_cipher_update.py
@@ -8,8 +8,8 @@ import os
 WP_URL = "https://blog.mexc.com/wp-json/wp/v2/posts"
 WP_USERNAME = os.getenv("WP_USERNAME")
 WP_APP_PASSWORD = os.getenv("WP_APP_PASSWORD")
-POST_ID = 295857  # ID bài Hamster Kombat Cipher Code
-CHECK_WORD = "INPUT"   # Word hiện có trên bài. Chỉ update khi scrape != CHECK_WORD
+POST_ID = 295692  # ID bài Hamster Kombat Cipher Code
+CHECK_WORD = "INPUTA"   # Word hiện có trên bài. Chỉ update khi scrape != CHECK_WORD
 
 # ================= SCRAPE SITE 1 =================
 def scrape_cipher_site1():
